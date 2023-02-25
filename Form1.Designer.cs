bb8a5fe8f40d5e303fcb064843d1bd54c88e5cee
Arch <garanov.iliya@gmail.com>
2023-02-26 01:03:56 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.bStop = new System.Windows.Forms.Button();
+            this.bStart = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.nudDen = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.nudResol = new System.Windows.Forms.NumericUpDown();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.newGenTimer = new System.Windows.Forms.Timer(this.components);
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudDen)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudResol)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            resources.ApplyResources(this.splitContainer1, "splitContainer1");
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.label4);
+            this.splitContainer1.Panel1.Controls.Add(this.label3);
+            this.splitContainer1.Panel1.Controls.Add(this.numericUpDown1);
+            this.splitContainer1.Panel1.Controls.Add(this.bStop);
+            this.splitContainer1.Panel1.Controls.Add(this.bStart);
+            this.splitContainer1.Panel1.Controls.Add(this.label2);
+            this.splitContainer1.Panel1.Controls.Add(this.nudDen);
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            this.splitContainer1.Panel1.Controls.Add(this.nudResol);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.splitContainer1.Panel2.Controls.Add(this.pictureBox2);
+            // 
+            // bStop
+            // 
+            this.bStop.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            resources.ApplyResources(this.bStop, "bStop");
+            this.bStop.Name = "bStop";
+            this.bStop.UseVisualStyleBackColor = false;
+            this.bStop.Click += new System.EventHandler(this.bStop_Click);
+            // 
+            // bStart
+            // 
+            this.bStart.BackColor = System.Drawing.Color.GreenYellow;
+            resources.ApplyResources(this.bStart, "bStart");
+            this.bStart.Name = "bStart";
+            this.bStart.UseVisualStyleBackColor = false;
+            this.bStart.Click += new System.EventHandler(this.bStart_Click);
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // nudDen
+            // 
+            resources.ApplyResources(this.nudDen, "nudDen");
+            this.nudDen.Maximum = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            this.nudDen.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.nudDen.Name = "nudDen";
+            this.nudDen.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // nudResol
+            // 
+            resources.ApplyResources(this.nudResol, "nudResol");
+            this.nudResol.Maximum = new decimal(new int[] {
+            25,
+            0,
+            0,
+            0});
+            this.nudResol.Minimum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.nudResol.Name = "nudResol";
+            this.nudResol.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            // 
+            // pictureBox2
+            // 
+            resources.ApplyResources(this.pictureBox2, "pictureBox2");
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox2_MouseMove);
+            // 
+            // newGenTimer
+            // 
+            this.newGenTimer.Interval = 5;
+            this.newGenTimer.Tick += new System.EventHandler(this.newGenTimer_Tick);
+            // 
+            // numericUpDown1
+            // 
+            resources.ApplyResources(this.numericUpDown1, "numericUpDown1");
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            5000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label4.Name = "label4";
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.Controls.Add(this.splitContainer1);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Name = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.nudDen)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudResol)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.NumericUpDown nudResol;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button bStop;
+        private System.Windows.Forms.Button bStart;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown nudDen;
+        private System.Windows.Forms.Timer newGenTimer;
+        public System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private Graphics graphics;
+        private int resolution;   //разрешение(маштаб)
+        private int rows;         //строки
+        private int columns;     //столбцы
+        private bool[,] field;   //массив координат, позже в нем же будут данные жива клетка или нет                 
+        uint currentGen;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();            
+        }
+
+        private void StartGame()            //первая генерация карты
+        {
+            if (newGenTimer.Enabled)
+                return;
+
+            currentGen = 0;
+            
+
+            nudResol.Enabled = false;                      //выключаются кнопки для изменения плотности населения и разрешения
+            nudDen.Enabled = false;
+            resolution = (int)nudResol.Value;            //разрешение записывается в переменную
+
+            rows = pictureBox2.Height / resolution;              //расчет того сколько клеток на поле
+            columns = pictureBox2.Width / resolution;           
+            field = new bool[columns, rows];
+
+            Random random = new Random();
+            for (int x = 0; x < columns; x++)
+            {
+                for (int y = 0; y < rows; y++)
+                {
+                    field[x, y] = random.Next((int)nudDen.Value) == 0;      //рандомные числа чтобы клетки были живыми или мертвыми
+                }
+            }
+
+            pictureBox2.Image = new Bitmap(pictureBox2.Width, pictureBox2.Height);
+            graphics = Graphics.FromImage(pictureBox2.Image);
+            newGenTimer.Start();
+        }
+
+        private void NextGen()         //отрисовка
+        {
+            graphics.Clear(Color.Black);
+            Text = $"Поколение + {++currentGen}";
+
+            var newField = new bool[columns, rows];
+
+            for (int x = 0; x < columns; x++)
+            {
+                for (int y = 0; y < rows; y++)
+                {
+                    var neighboursCount = countNeighbours(x, y);
+                    var hasLife = field[x, y];
+
+                    if (!hasLife && neighboursCount == 3)
+                    {
+                        newField[x, y] = true;
+                    }
+                    else if (hasLife && (neighboursCount < 2 || neighboursCount > 3))
+                    {
+                        newField[x, y] = false;
+                    }
+                    else
+                    {
+                        newField[x, y] = field[x, y];
+                    }
+
+                    if (hasLife)
+                    {
+                        graphics.FillRectangle(Brushes.Crimson, x * resolution, y * resolution, resolution -1, resolution -1);
+                    }
+                }
+            }
+            field = newField;
+            pictureBox2.Refresh();         //перезагрузка картики
+        }
+
+
+        private int countNeighbours(int x, int y) 
+        {
+            int count = 0;
+
+            for (int i = -1; i < 2; i++)
+            {
+                for (int j = -1; j < 2; j++)
+                {
+                    var col = (x + i + columns) % columns;
+                    var row = (y + j + rows) % rows;
+                    var isSelfChecking = col == x && row == y;
+                    var hasLife = field[col, row];
+                    if (hasLife && !isSelfChecking)                    
+                        count++;                    
+                }
+            }
+
+            return count;
+        }
+
+
+        private void StopGame()
+        {
+            if (!newGenTimer.Enabled)
+            {
+                return;
+            }
+            else
+            {
+                newGenTimer.Stop();
+                nudResol.Enabled = true;                      //включаются кнопки для изменения плотности населения и разрешения
+                nudDen.Enabled = true;
+            }       
+        }
+
+        private void bStop_Click(object sender, EventArgs e)        //кнопка стоп
+        {
+            StopGame();
+        }
+
+
+        private void newGenTimer_Tick(object sender, EventArgs e)  //тик таймера каждые пол секунды
+        {
+            NextGen();             
+        }
+
+
+        private void bStart_Click(object sender, EventArgs e)     //кнопка старт
+        {
+           StartGame();
+        }
+
+        private void pictureBox2_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (!newGenTimer.Enabled)
+                return;
+
+            if (e.Button == MouseButtons.Left)
+            {
+                var x = e.Location.X / resolution;
+                var y = e.Location.Y / resolution;                
+                var validationPassed = ValidateMousePos(x, y);
+                if (validationPassed)
+                    field[x, y] = true;
+            }
+
+            if (e.Button == MouseButtons.Right)
+            {
+                var x = e.Location.X / resolution;
+                var y = e.Location.Y / resolution;
+                var validationPassed = ValidateMousePos(x, y);
+                if (validationPassed)
+                    field[x, y] = false;
+            }
+        }
+        
+        private bool ValidateMousePos(int x, int y)
+        {
+            return x >= 0 && y >= 0 && x < columns && y < rows;
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            newGenTimer.Interval = (int)numericUpDown1.Value;
+        }
+    }
+}
